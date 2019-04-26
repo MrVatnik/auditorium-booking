105e77e767379da4bcb35fd88e3dadb2bdf17250
MrVatnik <zacvars@gmail.com>
2019-04-26 12:03:36 +0300

    added user class
    and some other minor changes

--- auditoriumBooking/src/Eltech/Vatnik/Booking.java
+++ auditoriumBooking/src/Eltech/Vatnik/Booking.java
@@ -9,7 +9,7 @@ public class Booking {
     Room room;
 
     Booking(String name, Date day, BookingTime time, Room room){
-        number = (int)Math.random();
+        number = (int)((((name.charAt(0)-'A'+5)*10+day.getDay()+4)*10+day.getMonth()+3)*10+time.ordinal()+2)*10 + room.ordinal();
         this.name = name;
         this.day = day;
         this.time = time;
@@ -17,7 +17,43 @@ public class Booking {
     }
 
     public String toString(){
-        return number + " " + name + " " + room + " " + day.getDate() +"."+day.getMonth()+"."+(day.getYear()+1900)+ " " + time;
+        return number + " " + name + " " + room + " " + day.getDate() +"."+(day.getMonth()+1)+"."+(day.getYear()+1900)+ " " + time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDay() {
+        return day;
+    }
+
+    public void setDay(Date day) {
+        this.day = day;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public BookingTime getTime() {
+        return time;
+    }
+
+    public void setTime(BookingTime time) {
+        this.time = time;
+    }
+
+    public static int getNumber() {
+        return number;
     }
 }
 
--- auditoriumBooking/src/Eltech/Vatnik/Main.java
+++ auditoriumBooking/src/Eltech/Vatnik/Main.java
@@ -6,7 +6,10 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-	Booking test = new Booking("Ivanov Ivan Ivanovitch",new Date(),BookingTime.STARTING1140,Room.RM_03);
+	    Booking test = new Booking("Ivanov Ivan Ivanovitch",new Date(),BookingTime.STARTING1140,Room.RM_03);
+	    System.out.println(test);
+	    //
+	    test.setDay(new Calendar.Builder().setDate(2019,-1,1).build().getTime());
         System.out.println(test);
     }
 }
